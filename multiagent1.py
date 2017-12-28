78e25338a94e1a557220604ec0bfbfa944fa5cd2
Jacob Cole <tmad4000@gmail.com>
2017-12-28 15:55:35 -0800

    obstacle oop

--- multiagent1.py
+++ multiagent1.py
@@ -31,16 +31,54 @@ class Car:
         self.x_changeLeft = -5
         self.x_changeRight = 5
         self.car_width = 73
+        self.speed=5
     
     def nudgeLeft(self):
         self.x += self.x_changeLeft
-
+    
     def nudgeRight(self):
         self.x += self.x_changeRight
+
+    def nudgeUp(self):
+        self.y -= self.speed
+    
+    def nudgeDown(self):
+        self.y += self.speed
+
 
     def draw(self):
         circle = pygame.draw.circle(gameDisplay, (0, 0, 0), (int(self.x), int(self.y)), int(self.car_width/2), 1)
         
+
+
+class Obstacle:
+    def __init__(self,x=display_width * 0.45, y=display_height * 0.8):
+        thing_startx = random.randrange(0, display_width)
+        thing_starty = -600
+        
+        self.x = thing_startx
+        self.y = thing_starty
+        self.color=black
+       
+        self.vx = 0
+        self.vy = 7
+
+        self.w = 100
+        self.h = 100
+
+
+    def tick(self): 
+        self.x += self.vx
+        self.y += self.vy
+
+        if self.y > display_height:
+            self.y = 0 - self.h
+            self.x = random.randrange(0,display_width)
+
+    def draw(self):        
+        # circle = pygame.draw.circle(gameDisplay, (0, 0, 0), (int(self.x), int(self.y)), int(self.car_width/2), 1)
+        # things(thingx, thingy, thingw, thingh, color):
+        pygame.draw.rect(gameDisplay, self.color, [int(self.x), int(self.y), int(self.w), int(self.h)])
 
 
         
@@ -51,8 +89,8 @@ def things(thingx, thingy, thingw, thingh, color):
     
 
 
-def car(x,y):
-    circle = pygame.draw.circle(gameDisplay, (0, 0, 0), (int(x), int(y)), int(car_width/2), 1)
+# def car(x,y):
+#     circle = pygame.draw.circle(gameDisplay, (0, 0, 0), (int(x), int(y)), int(car_width/2), 1)
 
     # gameDisplay.blit(carImg,(x,y))
 
@@ -79,6 +117,7 @@ def crash():
     
 def game_loop():
     theCar=Car()
+    obstacle=Obstacle()
     # x = (display_width * 0.45)
     # y = (display_height * 0.8)
 
@@ -95,6 +134,7 @@ def game_loop():
     while not gameExit:
 
 
+        #left and right motion
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_LEFT]:
         # if event.key == pygame.K_LEFT:
@@ -105,7 +145,15 @@ def game_loop():
             # x_change = 5
             theCar.nudgeRight()     
             
+        if pressed[pygame.K_UP]:                    
+            theCar.nudgeUp()     
+        if pressed[pygame.K_DOWN]:                    
+            theCar.nudgeDown()     
+            
+
         for event in pygame.event.get():
+
+            #wuitting
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
@@ -128,21 +176,24 @@ def game_loop():
             #         x_change = 0
 
         # x += x_change
+        
         gameDisplay.fill(white)
 
      ##########
         # things(thingx, thingy, thingw, thingh, color)
-        things(thing_startx, thing_starty, thing_width, thing_height, black)
-        thing_starty += thing_speed
+        # things(thing_startx, thing_starty, thing_width, thing_height, black)
+        obstacle.tick()
+        # thing_starty += thing_speed
         # car(x,y)
+        obstacle.draw()
         theCar.draw()
      ##########
         if theCar.x > display_width - theCar.car_width or theCar.x < 0:
             crash()
 
-        if thing_starty > display_height:
-            thing_starty = 0 - thing_height
-            thing_startx = random.randrange(0,display_width)
+        # if thing_starty > display_height:
+        #     thing_starty = 0 - thing_height
+        #     thing_startx = random.randrange(0,display_width)
             
         
         pygame.display.update()
